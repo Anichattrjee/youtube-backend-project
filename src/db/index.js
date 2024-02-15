5fa42a0d443d35d8ef6c8a10fd3beb5dfd0ab827
Aniruddha Chatterjee <anichat1999@gmail.com>
2024-02-15 21:07:39 +0530

    MongoDB DatabaseConecctionly without Express

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+//connecting database here
+ export const connectDB= async ()=>{
+    try{
+        const connectionInstance=await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+        // console.log(`${process.env.MONGODB_URL}/${DB_NAME}`)
+        console.log(`Connected to Video database: DB HOST: ${connectionInstance.connection.host}`);
+    }catch(error)
+    {
+        console.log("MONGODB connection error! ",error);
+    }
+};
+
